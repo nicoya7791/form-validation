9bd4fdd8edc2830c43c2fc9e26b1bcf5221ebc44
Henry <hablandon750@outlook.com>
2021-02-16 00:39:26 -0800

    completed section seven payment options

--- js/script.js
+++ js/script.js
@@ -5,7 +5,23 @@ const jobOptionSelection = document.querySelector('#title');
 const shirtsDesign = document.querySelector('#design');
 const shirtsColor = document.querySelector('#color');
 const activityBoxInput = document.querySelectorAll('#activities-box input');
+const paymentMethod = document.querySelectorAll('#payment option');
+const paypalOption = document.querySelector('#paypal');
+const bitcoinOption = document.querySelector('#bitcoin');
+const creditCardOption = document.querySelector('#credit-card');
+let payOptionArray=[paypalOption, creditCardOption, bitcoinOption];
 let totalCost = 0;
+//set credit card as default on pageload. Using for loop, so if more payments added will still work.
+for (let i = 0; i < paymentMethod.length; i++) {
+	let payId = paymentMethod[i].value;
+	if (payId==='credit-card') {
+		paymentMethod[i].selected =true;
+	};
+	
+}
+
+//by default shows credit card on load.
+creditCardOption.selected = true;
 //disable the shirt color dropdown manu.
 shirtsColor.disabled=true;
 // give focus to name Input
@@ -47,7 +63,9 @@ shirtsDesign.addEventListener('change', (e)=>{
 //event listener on activities field set.
 document.querySelector('#activities').addEventListener('change', (e)=>{
 	const clickedActivity = e.target;
+	//p is used to display the total value in the html
 	let p = document.querySelector('#activities-cost');
+	//cost of activity that is chosen
 	let clickedCost = clickedActivity.dataset.cost;
 	//if user chose an ativity, the price gets added to total, if user uncheck the activity, the 
 	//activity price gets deducted from total.
@@ -60,3 +78,20 @@ document.querySelector('#activities').addEventListener('change', (e)=>{
 	//displays the updated total cost.
 	p.textContent = (`Total: $${totalCost}`);
 })
+
+//event listener to show or hide pay options.
+document.querySelector('#payment').addEventListener('change', (e)=>{
+	let = payMethodClicked = e.target.value;
+	for (let i = 0; i < payOptionArray.length; i++) {
+			let element = payOptionArray[i].id;
+			if(payMethodClicked===element){
+				
+				payOptionArray[i].hidden = false;
+			}
+			else{
+				payOptionArray[i].hidden = true;
+			}
+				
+		}
+})
+
