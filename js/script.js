f2e9183f67d6005aaf58fd83407d7e5b688a82bf
Henry <hablandon750@outlook.com>
2021-02-16 23:05:55 -0800

    section 8 almost done, just need to add the if statements in the e.listerner

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // global variables
+const form = document.querySelector('form');
 const nameElement = document.querySelector('#name');
 const otherJobRole = document.querySelector('#other-job-role');
 const jobOptionSelection = document.querySelector('#title');
@@ -11,17 +12,7 @@ const bitcoinOption = document.querySelector('#bitcoin');
 const creditCardOption = document.querySelector('#credit-card');
 let payOptionArray=[paypalOption, creditCardOption, bitcoinOption];
 let totalCost = 0;
-//set credit card as default on pageload. Using for loop, so if more payments added will still work.
-for (let i = 0; i < paymentMethod.length; i++) {
-	let payId = paymentMethod[i].value;
-	if (payId==='credit-card') {
-		paymentMethod[i].selected =true;
-	};
-	
-}
 
-//by default shows credit card on load.
-creditCardOption.selected = true;
 //disable the shirt color dropdown manu.
 shirtsColor.disabled=true;
 // give focus to name Input
@@ -29,7 +20,7 @@ nameElement.focus();
 //hides the job role input on page load. if added during otherJobRole var declaration
 //it will overrides the e.listener.
 otherJobRole.hidden = true;
-//this event hide/diplay text field base on user job role selection
+//this event hide/diplay the 'other' job selection.
 jobOptionSelection.addEventListener('change', (e) => {
 	for (let i = 0; i < jobOptionSelection.length; i++) {
 		let jobSelection = jobOptionSelection[i].value;
@@ -45,7 +36,9 @@ jobOptionSelection.addEventListener('change', (e) => {
 //event listener to display color available base on the t-shirt theme.
 shirtsDesign.addEventListener('change', (e)=>{
 	const clicked = e.target;
+	//gets the value of the selected design.
 	const clickedValue = clicked.value;
+	//if t-shirt design is selected, color options are available for that specific design.
 	if(clickedValue){		
 		shirtsColor.disabled = false;
 		for (let i = 0; i < shirtsColor.length; i++) {
@@ -60,7 +53,7 @@ shirtsDesign.addEventListener('change', (e)=>{
 	}
 
 })
-//event listener on activities field set.
+//event listener on activities field set, add or substract cost of activities
 document.querySelector('#activities').addEventListener('change', (e)=>{
 	const clickedActivity = e.target;
 	//p is used to display the total value in the html
@@ -78,12 +71,31 @@ document.querySelector('#activities').addEventListener('change', (e)=>{
 	//displays the updated total cost.
 	p.textContent = (`Total: $${totalCost}`);
 })
+//function to show credit card as default payment and hides the other options.
+function defaultPaymentMethod(){
+	//set credit card as default on pageload. Using for loop, so if more payments added will still work.
+	
+	for (let i = 0; i < paymentMethod.length; i++) {
+		let payId = paymentMethod[i].value;
+		if (payId==='credit-card') {
+			paymentMethod[i].selected =true;
+		}
+	}
+	paypalOption.hidden=true;
+	bitcoinOption.hidden=true;
+	
+}
+//show default payment opting on page load.
+defaultPaymentMethod();
 
-//event listener to show or hide pay options.
+
+//event listener to show or hide payment options base on paymen method.
 document.querySelector('#payment').addEventListener('change', (e)=>{
 	let = payMethodClicked = e.target.value;
-	for (let i = 0; i < payOptionArray.length; i++) {
+		//loop through array of option and hides the the ones not selected.
+		for (let i = 0; i < payOptionArray.length; i++) {
 			let element = payOptionArray[i].id;
+			
 			if(payMethodClicked===element){
 				
 				payOptionArray[i].hidden = false;
@@ -91,7 +103,67 @@ document.querySelector('#payment').addEventListener('change', (e)=>{
 			else{
 				payOptionArray[i].hidden = true;
 			}
-				
 		}
+		
 })
+//=====================================================
+// ----------------FORM VALIDATION---------------------
+//=====================================================
+//validate name field
+const nameFieldValidator = ()=>{
+	const nameValue = nameElement.value;
+	const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*?[ -]?[a-zA-Z]*?$/.test(nameValue)
+	return nameIsValid;
+}
+//email validation, validate most common emails.
+const emailFieldValidator=()=>{
+	const email = document.querySelector('#email');
+	const emailValue = email.value;
+	const emailIsValid = /^[^@]+@[^@.]+\.[a-z]*$/i.test(emailValue);
+	return emailIsValid;
+}
+//activities validation, if no activity total should be zero
+const activiyValidator= ()=>{
+	const activityIsValid = totalCost > 0;
+	return activityIsValid;
+}
+//card number validator, number must be between 13 and 14 no spaces or dashes.
+const cardNumberValidator = ()=>{
+	const cardNumber= document.querySelector('#cc-num')
+	const cardNumberValue = cardNumber.value;
+	const cardNumberIsValid = /^\d{13,16}$/.test(cardNumberValue);
+	return cardNumberIsValid;
+}
+//zip code must be 5 digits no space or dash allowed
+const zipCodeValidator=()=>{
+	const zipCode = document.querySelector('#zip');
+	const zipCodeValue = zipCode.value;
+	const zipIsValid = /^\d{5}$/.test(zipCodeValue);
+	return zipIsValid;
+}
+//cvv must be 3 digtis no space or dashes allowed.
+const cvvValidator = ()=>{
+	const cvv = document.querySelector('#cvv');
+	const cvvValue = cvv.value;
+	const cvvIsValid= /^\d{3}$/.test(cvvValue);
+	return cvvIsValid;
+}
+//form event listener, conditioned if all required field are true.
+form.addEventListener('submit', (e)=>{
+	if(!nameFieldValidator()){
+		e.preventDefault();
+		console.log('name not valid');
+	}
+	if(!emailFieldValidator()){
+		e.preventDefault();
+		console.log('email not valid');
+	}
+	if(!activiyValidator()){
+		e.preventDefault();
+		console.log('activity not valid');
+	}
 
+	
+	
+
+})
